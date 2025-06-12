ccdb3793eb502fa79cab66882914fbe3e3af3c5c
兩河流域 <teariver0220@gmail.com>
2025-06-13 03:01:56 +0800

    trace.h relayout

--- trace.h
+++ trace.h
@@ -3,16 +3,23 @@
 
 #include <stdbool.h>
 
-#define TRACE_ARGS const char *file, const char *func, int line
-
-#define GET_TRACE __FILE__, __func__, __LINE__
-
 //
-// trace
+/*    impl    */
+
+#define TRACE_ARGS const char *file, const char *func, int line
 
 void trace_impl_push(TRACE_ARGS);
 
 void trace_impl_pop(void);
+
+void panic_impl(TRACE_ARGS, bool expr, const char* fmt, ...);
+
+#undef TRACE_ARGS
+
+//
+/*    api    */
+
+#define GET_TRACE __FILE__, __func__, __LINE__
 
 #define TRACE(expr)                 \
     do {                            \
@@ -21,17 +28,10 @@ void trace_impl_pop(void);
         trace_impl_pop();           \
     } while (0)
 
-//
-// panic
-
-void panic_impl(TRACE_ARGS, bool expr, const char* fmt, ...);
-
 #define PANIC(expr, msg) panic_impl(GET_TRACE, (expr), (msg));
 
 #define PANIC_F(expr, fmt, ...) panic_impl(GET_TRACE, (expr), (fmt), __VA_ARGS__)
 
 #define PANIC_NULL(ptr) PANIC_F((ptr), "pointer \"%s\" is NULL", #ptr)
-
-#undef TRACE_ARGS
 
 #endif
